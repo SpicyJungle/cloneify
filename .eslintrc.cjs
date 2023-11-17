179238e8c71a56ce4cd685d5fe30781237c3a377
Marius Eikenes <eikenes.marius@gmail.com>
2023-11-17 09:47:44 +0100

    Fix: Deployment issues 2

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,13 +21,13 @@ const config = {
   plugins: ["@typescript-eslint"],
   extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
   rules: {
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
-      {
-        prefer: "type-imports",
-        fixStyle: "inline-type-imports",
-      },
-    ],
+    // "@typescript-eslint/consistent-type-imports": [
+    //   "warn",
+    //   {
+    //     prefer: "type-imports",
+    //     fixStyle: "inline-type-imports",
+    //   },
+    // ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
   },
 };
